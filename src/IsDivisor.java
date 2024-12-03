c48575c8d4fe7104c3781e526861e955ae016d9f
ArtemKarDev <akm86@mail.ru>
2024-12-03 20:10:10 +0300

    delete  isUpperCase class

--- src/IsDivisor.java
+++ src/IsDivisor.java
@@ -4,7 +4,7 @@ public class IsDivisor {
 
     public static void main(String[] args) {
 
-        System.out.println("Введите числа, нажмите <Enter>: ");
+        System.out.println("Введите числ, нажмите <Enter>: ");
         int a = new Scanner(System.in).nextInt();
         int b = new Scanner(System.in).nextInt();
         System.out.println(String.format("Данные числа %d и %d, делят друг друга на цело? : ", a, b) + isDivisor(a, b));
