45c726c2f14fe21b70700d7ab57d480921d479ec
ArtemKarDev <akm86@mail.ru>
2024-12-05 18:15:41 +0300

    edit СharToNum class

--- src/СharToNum.java
+++ src/СharToNum.java
@@ -6,12 +6,12 @@ public class СharToNum {
     System.out.println("Введите символ и нажмите <Enter>: ");
     String str = new Scanner(System.in).next();
     char character = str.charAt(0);
-    System.out.println("Данный символ имеет код: " + charToNum(character));
+    System.out.println("Данный символ это цифра: " + charToNum(character));
     }
 
     public static int charToNum(char x){
         int code = (int)x;
-        return code;
+        return code - 48;
     }
 
 }
