2ff9159e50971e40df2e7439f6ea81aa5f23c6f2
ArtemKarDev <akm86@mail.ru>
2024-12-06 08:44:08 +0300

    added is35 class

---
+++ src/Is35.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Is35 {
+    public static void main(String[] args) {
+        System.out.println("Введите число х: ");
+        int x = new Scanner(System.in).nextInt();
+        System.out.println(String.format("результат: %b", is35(x)));
+    }
+    public static boolean is35(int x){
+        if (x % 3 == 0 && x % 5 == 0) {return false;}
+        else return x % 3 == 0 || x % 5 == 0;
+    }
+}
+
