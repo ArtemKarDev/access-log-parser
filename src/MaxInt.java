6c85b9783e65f246e1a877c3876e8aa3b036a417
ArtemKarDev <akm86@mail.ru>
2024-12-06 07:21:52 +0300

    added maxInt class

--- src/MaxInt.java
+++ src/MaxInt.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
 
-
-public class SafeDiv {
+public class MaxInt {
 
     public static void main(String[] args) {
         System.out.println("Введите числа х и у: ");
         int x = new Scanner(System.in).nextInt();
         int y = new Scanner(System.in).nextInt();
-        System.out.println(String.format("Частное чисел равно: %d ", safeDiv(x, y)));
+        System.out.println(String.format("Максимальное число: %d ", max(x, y)));
     }
 
-    public static int safeDiv(int x, int y){
-        if (y>0) return x/y;
-        return 0;
+    public static int max(int x, int y){
+        if (x>y) return x;
+        return y;
     }
 }
 
