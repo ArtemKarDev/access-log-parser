df324907ba243e7b99d8f26189ee8a8e252fff9e
ArtemKarDev <akm86@mail.ru>
2024-12-11 12:27:42 +0300

    added cycle get path file

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,35 @@
+import java.io.File;
 import java.lang.Math;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+            Scanner sc = new Scanner(System.in);
+            String path;
+            boolean fileExists;
+            boolean isDirectory;
+            int count = 0;
+
+        while (true) {
+            System.out.println("Введите путь к файлу и нажмите <Enter>: ");
+            path = sc.nextLine();
+            File file = new File(path);
+            fileExists = file.exists();
+            isDirectory = file.isDirectory();
+            if (isDirectory){
+                System.out.println("Указан неполный путь к файлу!");
+                continue;
+            }
+            if (fileExists){
+                System.out.println("Путь указан верно.");
+                count = count + 1;
+                System.out.println("Это файл номер " + count);
+            }
+            else  {
+                System.out.println("Указан несуществующий файл в папке!");
+            }
+
+        }
     }
 }
