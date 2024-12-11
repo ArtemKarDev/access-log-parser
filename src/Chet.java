e991e0c48c7d63ba4927b1f30e98ef34e891a582
ArtemKarDev <akm86@mail.ru>
2024-12-11 06:35:21 +0300

    added chet class

--- src/Chet.java
+++ src/Chet.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class ReverseListNums {
+public class Chet {
     public static void main(String[] args) {
         System.out.println("Введите число х: ");
         int x = new Scanner(System.in).nextInt();
-        System.out.println(String.format("результат: %s", reverseListNums(x)));
+        System.out.println(String.format("результат: %s", chet(x)));
     }
-    public static String reverseListNums(int x){
+    public static String chet(int x){
         String res ="";
-        for (int i=x; i>=0; i-- ){
+        for (int i=0; i<=x; i+=2 ){
             res += i + " ";
         }
         return  res;
