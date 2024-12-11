9357c3b07a9f6e7ea10e019f299a6fbba8172920
ArtemKarDev <akm86@mail.ru>
2024-12-11 08:29:08 +0300

    added leftTriangle class

--- src/LeftTriangle.java
+++ src/LeftTriangle.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
-public class Square {
+public class LeftTriangle {
     public static void main(String[] args) {
         System.out.println("Введите число х: ");
         int x = new Scanner(System.in).nextInt();
         //System.out.printf("результат: %b", equalNum(x));
-        square(x);
+        leftTriangle(x);
     }
-    public static void square(int x){
+    public static void leftTriangle(int x){
         for (int i=1; i<=x;i++){
-            for (int j=1; j<=x ;j++ ){
+            for (int j=1; j<=i ;j++ ){
                 System.out.print("*");
             }
             System.out.println();
