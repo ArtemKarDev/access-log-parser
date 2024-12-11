073a56664dafdd915b829b8b5a23ae9a7e9f4904
ArtemKarDev <akm86@mail.ru>
2024-12-11 08:37:04 +0300

    added rightTriangle class

--- src/RightTriangle.java
+++ src/RightTriangle.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 
-public class LeftTriangle {
+public class RightTriangle {
     public static void main(String[] args) {
         System.out.println("Введите число х: ");
         int x = new Scanner(System.in).nextInt();
         //System.out.printf("результат: %b", equalNum(x));
-        leftTriangle(x);
+        rightTriangle(x);
     }
-    public static void leftTriangle(int x){
-        for (int i=1; i<=x;i++){
-            for (int j=1; j<=i ;j++ ){
-                System.out.print("*");
+    public static void rightTriangle(int x){
+        for (int i=x; i>=1; i--){
+            for (int j=1; j<=x ; j++ ){
+                if(j<i) System.out.print(" ");
+                else    System.out.print("*");
             }
             System.out.println();
         }
