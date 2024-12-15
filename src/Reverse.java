7b0031b163f87c88a8b1022b2b5219be19aca1b2
ArtemKarDev <akm86@mail.ru>
2024-12-15 21:43:10 +0300

    adite reverse class

--- src/Reverse.java
+++ src/Reverse.java
@@ -2,19 +2,28 @@ import java.util.Arrays;
 
 public class Reverse {
     public static void main(String[] args) {
-        int[] arr = {-3,-2,-1,0,1,2,3};
+        int[] arr = {-3,-2,-1,0,1,2,3,4};
 //        System.out.println("Input x: ");
 //        int x = new Scanner(System.in).nextInt();
         reverse(arr);
         System.out.println("Response: "+ Arrays.toString(arr));
     }
 
-    public static void reverse(int[] arr){
-        int[] arrTemp = new int[arr.length];
-        System.arraycopy(arr, 0, arrTemp, 0, arr.length);
+    public static int[] reverseBack(int[] arr){
+        int[] arrRev = new int[arr.length];
         for (int i=0; i<arr.length; i++){
-            arr[i] = arrTemp[arr.length- 1 - i];
+            arrRev[i] = arr[arr.length- 1 - i];
         }
+        return arrRev;
     }
 
+
+    public static void reverse(int[] arr){
+        int temp = 0;
+        for (int i=0; i<arr.length/2; i++){
+            temp = arr[i];
+            arr[i] = arr[arr.length-1-i];
+            arr[arr.length-1-i] = temp;
+        }
+    }
 }
