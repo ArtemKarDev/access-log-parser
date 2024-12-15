0974a4b759f991200c4cba8634815bdde902cca4
ArtemKarDev <akm86@mail.ru>
2024-12-15 21:43:53 +0300

    edited reverse class

--- src/Reverse.java
+++ src/Reverse.java
@@ -19,7 +19,7 @@ public class Reverse {
 
 
     public static void reverse(int[] arr){
-        int temp = 0;
+        int temp;
         for (int i=0; i<arr.length/2; i++){
             temp = arr[i];
             arr[i] = arr[arr.length-1-i];
