2f6960c324d3a1c85ac38bb8ef3a4dbee4e0e74e
ArtemKarDev <akm86@mail.ru>
2024-12-15 22:49:23 +0300

    added FindAll class

--- src/FindAll.java
+++ src/FindAll.java
@@ -1,13 +1,28 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class FindFirstAndLAst {
+public class FindAll {
     public static void main(String[] args) {
         int[] arr = {3,2,3,4,8,6,-9,3};
         System.out.println("Input x: ");
         int x = new Scanner(System.in).nextInt();
+        System.out.println("index: "+ Arrays.toString(findAll(arr, x)));
         System.out.println("index: "+ findFirst(arr, x));
         System.out.println("index: "+ findLast(arr, x));
 
+    }
+
+    public static int[] findAll(int[] arr, int x){
+        int[] temp = new int[arr.length];
+        int j = 0;
+        for (int i=0; i<=arr.length-1; i++){
+            if (x == arr[i]) { temp[j++] = i;}
+        }
+        int[] res = new int[j];
+        for(int i=0;i<res.length; i++){
+            res[i] = temp[i];
+        }
+        return res;
     }
 
     public static int findFirst(int[] arr, int x){
