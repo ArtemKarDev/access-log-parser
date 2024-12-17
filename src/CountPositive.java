5d05ee6b83ad410fb6229344cefde16bfb777372
ArtemKarDev <akm86@mail.ru>
2024-12-17 20:06:01 +0300

    edited countPositive class

--- src/CountPositive.java
+++ src/CountPositive.java
@@ -1,6 +1,6 @@
 public class CountPositive {
     public static void main(String[] args) {
-        int[] arr = {-3,-2,-3,-4,-8,-6,-9,-3};
+        int[] arr = {3};
 //        System.out.println("Input x: ");
 //        int x = new Scanner(System.in).nextInt();
         System.out.println("Response: "+ countPositive(arr));
@@ -10,7 +10,7 @@ public class CountPositive {
 
     public static int countPositive(int[] arr){
         int res = 0;
-        for (int i=0; i<arr.length-1; i++){
+        for (int i=0; i<arr.length; i++){
             if (arr[i]>0){ res += 1;}
         }
         return res;
