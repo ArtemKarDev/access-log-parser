0c317b803092cdcdac435cfa63f104f303ef719b
ArtemKarDev <akm86@mail.ru>
2024-12-17 19:23:48 +0300

    added Point class

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,13 @@
-import java.io.File;
-import java.lang.Math;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-            Scanner sc = new Scanner(System.in);
-            String path;
-            boolean fileExists;
-            boolean isDirectory;
-            int count = 0;
+        Point a1 = new Point(1,3);
+        Point a2 = new Point(1,3);
+        Point a3 = new Point(5,8);
+        System.out.println("points: "+a1+" "+a2+" "+a3);
+        System.out.println("compare a1 a2: "+(a1==a2));
+        System.out.println("compare a2 a3: "+(a2==a3));
 
-        while (true) {
-            System.out.println("Введите путь к файлу и нажмите <Enter>: ");
-            path = sc.nextLine();
-            File file = new File(path);
-            fileExists = file.exists();
-            isDirectory = file.isDirectory();
-            if (isDirectory){
-                System.out.println("Указан неполный путь к файлу!");
-                continue;
-            }
-            if (fileExists){
-                System.out.println("Путь указан верно.");
-                count = count + 1;
-                System.out.println("Это файл номер " + count);
-            }
-            else  {
-                System.out.println("Указан несуществующий файл в папке!");
-            }
-
-        }
     }
 }
