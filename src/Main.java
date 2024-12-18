d4635d2a030669db5e02ec06a2c88cd7e607f047
ArtemKarDev <akm86@mail.ru>
2024-12-18 15:50:21 +0300

    added Line class

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,22 @@ public class Main {
     public static void main(String[] args) {
 
         Point a1 = new Point(1,3);
-        Point a2 = new Point(1,3);
-        Point a3 = new Point(5,8);
-        System.out.println("points: "+a1+" "+a2+" "+a3);
-        System.out.println("compare a1 a2: "+(a1==a2));
-        System.out.println("compare a2 a3: "+(a2==a3));
+        Point a2 = new Point(5,8);
+
+        Line l1 = new Line(a1,a2);
+        Line l2 = new Line(10,11, 15,19);
+        Line l3 = new Line(l1.pEnd,l2.pStart);
+
+        System.out.println(l3);
+
+        a2.x = 116;
+        a2.y = 115;
+        l2.pStart.x = 119;
+        l2.pStart.y = 117;
+
+        System.out.println(l3);
+
+        System.out.println("Длина 3х линий: "+(l1.getLength()+l2.getLength()+l3.getLength()));
 
     }
 }
--- src/Point.java
+++ src/Point.java
@@ -6,6 +6,7 @@ public class Point {
         this.y = y;
     }
 
+
     public String toString(){
         return "{"+x+";"+y+"}";
     }
