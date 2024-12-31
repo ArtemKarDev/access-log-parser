233f518833a019097073b3a9808e54851b932b1a
ArtemKarDev <akm86@mail.ru>
2024-12-31 09:57:51 +0300

    edited Fraction class

--- src/Fraction.java
+++ src/Fraction.java
@@ -17,7 +17,6 @@ public class Fraction {
         return new Fraction( this.numerator*(commonDenominator/this.denominator) - fraction2.numerator*(commonDenominator/fraction2.denominator),commonDenominator);
     }
 
-
     private int gcdRecursionAlgorithm(int a, int b) {
         if (b == 0) {
             return a;
@@ -29,19 +28,6 @@ public class Fraction {
         return a / gcdRecursionAlgorithm(a, b) * b;
     }
 
-//    public int commonDenominator(Fraction fraction1, Fraction fraction2){
-//        int commonDenominator;
-//        int a = fraction1.denominator;
-//        int b = fraction2.denominator;
-//        for(int i = Math.max(a,b); i<= a*b; i += Math.max(a,b)){
-//            System.out.println(a+" "+b+" "+i);
-//            if (i%a == 0 && i%b == 0){
-//                commonDenominator = i;
-//                return commonDenominator;
-//            }
-//        }
-//        return a*b;
-//    }
 
     public String toString(){
         return this.numerator+"/"+this.denominator;
