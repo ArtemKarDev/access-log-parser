2093ea4ccfd2fe53754596a31b384ecd5603d9e6
ArtemKarDev <akm86@mail.ru>
2025-01-02 10:27:35 +0300

    added Square class

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        Fraction f1 = new Fraction(1,3);
-        Fraction f2 = new Fraction(2,5);
-        Fraction f3 = new Fraction(7,8);
-        System.out.println(f1+ " + "+ f2 +" - "+ f3 +" = "+ f1.sum(f2).minus(f3));
-        System.out.println(f1.sum(3).minus(2));
+        Square s1 = new Square(4,4,4);
+        System.out.println(s1);
+        s1.setX(0).setY(0).setSideLength(2);
+        System.out.println(s1);
 
     }
 }
