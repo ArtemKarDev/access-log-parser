56141e59197bde71137c68cff635af86930008ba
ArtemKarDev <akm86@mail.ru>
2025-01-21 19:33:21 +0300

    create method myPow

--- src/ru/courses/main/Main.java
+++ src/ru/courses/main/Main.java
@@ -1,9 +1,30 @@
+package ru.courses.main;
+
 import ru.stepup.karlashov.animal.Bird;
 import ru.stepup.karlashov.animal.Singable;
 import ru.stepup.karlashov.geometry.Areable;
 import ru.stepup.karlashov.geometry.Measurable;
 
+import java.util.Scanner;
+
+import static java.lang.Math.pow;
+import static java.lang.Integer.parseInt;
 public class Main {
+    public static void main(String[] args) {
+        String x, y;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        x = sc.next();
+        System.out.print("Введите число: ");
+        y = sc.next();
+
+        System.out.println(myPow(x,y));
+
+    }
+
+    protected static int myPow(String x, String y){
+         return (int) pow(parseInt(x), parseInt(y));
+    }
 
 
     public static double sumAllAreas(Areable... objects){
@@ -29,6 +50,4 @@ public class Main {
     public static void birdMarket(Singable object){
         object.sing();
     }
-
-
 }
--- src/ru/сourses/main/Start.java
+++
@@ -1,7 +0,0 @@
-package ru.сourses.main;
-
-public class Start {
-    public static void main(String[] args) {
-
-    }
-}
