542af4c78bdd887953bc9f99f6e441d984e6a299
ArtemKarDev <akm86@mail.ru>
2025-01-27 17:12:02 +0300

    create class Sum with exception

---
+++ src/Sum.java
@@ -0,0 +1,18 @@
+public class Sum {
+    public static void main(String[] args){
+
+        double sum = 0;
+
+        for(String arg : args){
+            try {
+                double number = Double.parseDouble(arg);
+                sum += number;
+            } catch (NumberFormatException e){
+                System.out.println("Некорректное значение " + arg + " (считаем за 0)");
+            }
+        }
+
+        System.out.println("Результат: " + sum);
+    }
+
+}
