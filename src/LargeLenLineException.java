6a989bc053b6bfbbd2d78c58572203bf44213d70
ArtemKarDev <akm86@mail.ru>
2025-01-28 10:44:12 +0300

    add method getInfo and custom exception

---
+++ src/LargeLenLineException.java
@@ -0,0 +1,14 @@
+public class LargeLenLineException extends RuntimeException{
+    public LargeLenLineException(){
+        super();
+    }
+    public LargeLenLineException(String message){
+        super(message);
+    }
+    public LargeLenLineException(String message, Throwable cause){
+        super(message, cause);
+    }
+    public LargeLenLineException(Throwable cause){
+        super(cause);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,57 @@
-import java.io.File;
-import java.lang.Math;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-            Scanner sc = new Scanner(System.in);
-            String path;
-            boolean fileExists;
-            boolean isDirectory;
-            int count = 0;
+        System.out.println("Введите путь к файлу и нажмите <Enter>: ");
+        // C:\aqa_inno\AccessLogParser\access.log
+        String path = new Scanner(System.in).nextLine();
+        File file = new File(path);
+        boolean fileExists  = file.exists();
+        boolean isDirectory = file.isDirectory();
 
-        while (true) {
-            System.out.println("Введите путь к файлу и нажмите <Enter>: ");
-            path = sc.nextLine();
-            File file = new File(path);
-            fileExists = file.exists();
-            isDirectory = file.isDirectory();
-            if (isDirectory){
-                System.out.println("Указан неполный путь к файлу!");
-                continue;
-            }
-            if (fileExists){
-                System.out.println("Путь указан верно.");
-                count = count + 1;
-                System.out.println("Это файл номер " + count);
-            }
-            else  {
-                System.out.println("Указан несуществующий файл в папке!");
-            }
-
+        if (isDirectory) {
+            System.out.println("Указан неполный путь к файлу!");
+        } else if (!fileExists) {
+            System.out.println("Указан несуществующий файл в папке!");
+        } else {
+            System.out.println("Путь указан верно.");
+            System.out.println(getInfo(path));
         }
+
     }
+
+
+    public static String getInfo(String path) throws IOException,LargeLenLineException {
+        int countLines = 0;
+        int maxLenLine = 0;
+        int minLenLine = 0;
+        int currentLenLine = 0;
+        String line;
+
+        FileReader fileReader = new FileReader(path);
+        BufferedReader reader = new BufferedReader(fileReader);
+
+        if ((line = reader.readLine()) != null) {
+            currentLenLine = line.length();
+            maxLenLine = currentLenLine;
+            minLenLine = currentLenLine;
+            countLines += 1;
+        }
+        while ((line = reader.readLine()) != null) {
+            countLines += 1;
+            currentLenLine = line.length();
+            if (currentLenLine > 1024){
+                throw new LargeLenLineException("Строка "+ countLines +" превышает 1024 символов!");
+            }
+            if (currentLenLine > maxLenLine){maxLenLine = currentLenLine;}
+            if (currentLenLine < minLenLine){minLenLine = currentLenLine;}
+        }
+        return "Количество строк: " + countLines + ". Максимальная строка: " + maxLenLine+ ". Минимальная строка: " +minLenLine;
+    }
+
+
+
+
 }
